ff018dfcc153bfda72cc33b14d28d423c4623761
Ida Alin <dev.7rip7ych@gmail.com>
2025-09-17 17:54:21 +0200

    Update imports

--- src/router.jsx
+++ src/router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, useParams } from "react-router-dom";
 import Index from "./views/Index";
 import Doc from "./views/Doc";
 
