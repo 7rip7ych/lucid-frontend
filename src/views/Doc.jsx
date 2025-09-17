2cd3d724c3493266d359e087075b2882f430a401
Ida Alin <dev.7rip7ych@gmail.com>
2025-09-17 17:30:49 +0200

    Move forms into separate components

--- src/views/Doc.jsx
+++ src/views/Doc.jsx
@@ -1,38 +1,16 @@
 import Header from './includes/header'
 import Footer from './includes/footer'
+import AddForm from './AddForm';
+import UpdateForm from './UpdateForm';
 
 function Doc() {
 
     return (
         <>
-        {/* <%- include('includes/header'); %> */}
         <Header />
         <main className="main" id="main">
-
-        <h2>Dokument</h2>
-        <form method="POST" action="/" className="new-doc">
-            <label for="title">Titel</label>
-            <input type="text" name="title" value="<%= doc?.title %>" />
-
-            <label for="content">Innehåll</label>
-            <textarea name="content"><%= doc?.content %></textarea>
-
-            <input type="submit" value="Skapa" />
-        </form>
-        
-        <form method="POST" action="/update" className="new-doc">
-            <label for="id">Id</label>
-            <input type="text" name="id" value="<%= id %>" />
-
-            <label for="title">Titel</label>
-            <input type="text" name="title" value="<%= doc?.title %>" />
-
-            <label for="content">Innehåll</label>
-            <textarea name="content"><%= doc?.content %></textarea>
-
-            <input type="submit" value="Uppdatera" />
-        </form>
-        {/* <%- include('includes/footer'); %> */}
+        <AddForm />
+        <UpdateForm />
         </main>
         <Footer />
         </>
