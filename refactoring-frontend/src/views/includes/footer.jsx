8f1bb78d3ce6c2138a47fa0e27f0aa06f0488bfa
lucation <lucasdanielssonvalladares@gmail.com>
2025-09-17 08:50:46 -0500

    Footer component

---
+++ refactoring-frontend/src/views/includes/footer.jsx
@@ -0,0 +1,15 @@
+
+function Footer() {
+
+    return (
+        <>
+
+        <footer>
+            <p class="copyright">&copy; Emil Folino</p>
+        </footer>
+
+        </>
+    );
+}
+
+export default Footer
