4b1e7242afcb864017c7d995fd55e8e8383005ce
Ida Alin <dev.7rip7ych@gmail.com>
2025-09-17 17:24:03 +0200

    Capitalize first letter of filename

---
+++ src/views/Index.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect} from "react";
+import Header from './includes/header'
+import Footer from './includes/footer'
+import documents from './models/docs'
+
+function Index() {
+    const [docs, setDocs] = useState([]);
+
+    useEffect(() => {
+        const loadData = async () => {
+            const allDocsData = await documents.allDocuments();
+            setDocs(allDocsData);
+        };
+
+        loadData();
+    }, []);
+    
+    return (
+        <>
+        {/* <%- include('includes/header'); %> */}
+        <Header />
+        <main className="main" id="main">
+
+        <h2>Dokument</h2>
+            {docs.map((doc) => (
+                <a href={`/${doc._id}`}>{doc.title}</a>
+            ))}
+        {/* <%- include('includes/footer'); %> */}
+        </main>
+        <Footer />
+        </>
+    );
+}
+
+export default Index
