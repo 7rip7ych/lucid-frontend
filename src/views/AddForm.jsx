c6003e5cb2e6da072bc05d49a5f1e56e04e27816
Ida Alin <dev.7rip7ych@gmail.com>
2025-09-17 17:41:53 +0200

    Implement properties

--- src/views/AddForm.jsx
+++ src/views/AddForm.jsx
@@ -1,13 +1,13 @@
-function AddForm() {
+function AddForm(props) {
     return (
         <>
         <h2>Dokument</h2>
         <form method="POST" action="/" className="new-doc">
             <label for="title">Titel</label>
-            <input type="text" name="title" value="<%= doc?.title %>" />
+            <input type="text" name="title" value={props.doc.title} />
 
             <label for="content">Innehåll</label>
-            <textarea name="content"><%= doc?.content %></textarea>
+            <textarea name="content">{props.doc.content}</textarea>
 
             <input type="submit" value="Skapa" />
         </form>
--- src/views/UpdateForm.jsx
+++ src/views/UpdateForm.jsx
@@ -1,15 +1,15 @@
-function UpdateForm() {
+function UpdateForm(props) {
     return (
         <>
         <form method="POST" action="/update" className="new-doc">
             <label for="id">Id</label>
-            <input type="text" name="id" value="<%= id %>" />
+            <input type="text" name="id" value={props.doc._id} />
 
             <label for="title">Titel</label>
-            <input type="text" name="title" value="<%= doc?.title %>" />
+            <input type="text" name="title" value={props.doc.title} />
 
             <label for="content">Innehåll</label>
-            <textarea name="content"><%= doc?.content %></textarea>
+            <textarea name="content">{props.doc.content}</textarea>
 
             <input type="submit" value="Uppdatera" />
         </form>
