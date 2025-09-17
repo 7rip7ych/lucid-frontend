262a10ba42feb29e6be0624320369bf452b81c64
Ida Alin <dev.7rip7ych@gmail.com>
2025-09-17 17:44:36 +0200

    Implement props and get data

--- src/views/Doc.jsx
+++ src/views/Doc.jsx
@@ -2,15 +2,16 @@ import Header from './includes/header'
 import Footer from './includes/footer'
 import AddForm from './AddForm';
 import UpdateForm from './UpdateForm';
+import documents from './models/docs';
 
-function Doc() {
-
+function Doc(props) {
+    let docu = documents.getOneDoc(props.id);
     return (
         <>
         <Header />
         <main className="main" id="main">
-        <AddForm />
-        <UpdateForm />
+        <AddForm doc={docu} />
+        <UpdateForm doc={docu} />
         </main>
         <Footer />
         </>
