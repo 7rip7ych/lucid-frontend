d1b894567a65b90c0ffbb9d2e34eb14368721d97
lucation <lucasdanielssonvalladares@gmail.com>
2025-09-22 18:38:31 -0500

    Added tests to Doc.jsx

--- src/tests/Doc.test.jsx
+++ src/tests/Doc.test.jsx
@@ -1,11 +1,17 @@
 import Doc from '../views/Doc';
 import { render, screen } from '@testing-library/react';
 import { describe, test, expect } from 'vitest';
+import { MemoryRouter } from "react-router-dom";
 
 
 describe('Doc', () => {
     test('Testing if renders heading', () => {
-        render(<Doc />);
+        //Need to render component with som type of router.
+        render(
+            <MemoryRouter>
+                <Doc />
+            </MemoryRouter>
+        );
         
         const h2Element = screen.getByRole('heading', { level: 2 });
         
@@ -13,12 +19,35 @@ describe('Doc', () => {
         expect(h2Element).toHaveTextContent('Dokument');
     });
 
-    // test('Testing if form have buttons', () => {
-    // render(<Doc />);
+    test('Testing if component renders right labels', () => {
+        //Need to render component with som type of router.
+        render(
+            <MemoryRouter>
+                <Doc />
+            </MemoryRouter>
+        );
+        
+        const label1Element = screen.getByText('Id');
+        const label2Element = screen.getByText('Titel');
+        const label3Element = screen.getByText('Innehåll');
+
+        expect(label1Element).toBeInTheDocument();
+        expect(label2Element).toBeInTheDocument();
+        expect(label3Element).toBeInTheDocument();
+    });
+
+    test('Testing if form have all the buttons', () => {
+        render(
+            <MemoryRouter>
+                <Doc />
+            </MemoryRouter>
+        );
     
-    // const labelElement = screen.getByRole('heading', { level: 2 });
-    
-    // expect(h2Element).toBeInTheDocument();
-    // expect(h2Element).toHaveTextContent('Dokument');
-    // });
+    const buttons = screen.getAllByRole('button');
+    expect(buttons).toHaveLength(3);
+
+    expect(screen.getByRole('button', { name: 'Skapa' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Uppdatera' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Radera' })).toBeInTheDocument();
+    });
 });
