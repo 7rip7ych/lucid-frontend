2cac3aba39318d4e9e3669d20833fcab2448dbd0
lucation <lucasdanielssonvalladares@gmail.com>
2025-09-22 12:23:24 -0500

    Added test environment to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
->>>>>>> origin/main
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/tests/setup.js',
   },
 });
-=======
-})
->>>>>>> origin/main
