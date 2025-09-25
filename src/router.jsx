4060dc7b021757597ae76205f092c9248d55b392
Ida Alin <dev.7rip7ych@gmail.com>
2025-09-25 13:50:20 +0200

    Try changing routes

--- src/router.jsx
+++ src/router.jsx
@@ -6,8 +6,8 @@ export default function App() {
     return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/:id" element={<Doc />} />
+            <Route path="/lucid-frontend/" element={<Index />} />
+            <Route path="/lucid-frontend/:id" element={<Doc />} />
         </Routes>
     </BrowserRouter>
     );
