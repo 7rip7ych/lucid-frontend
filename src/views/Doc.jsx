1ab7f9e8bc4de8da68db5e9931a54340ca50b1a8
Ida Alin <dev.7rip7ych@gmail.com>
2025-10-02 16:52:26 +0200

    Move socket to Editor component

--- src/views/Doc.jsx
+++ src/views/Doc.jsx
@@ -1,15 +1,10 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect} from "react";
-import { io } from 'socket.io-client';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import imgUrl from '../assets/skateboard.gif';
 import documents from './models/docs';
-import Form from './components/Form';
-
-const SERVER_URL = "http://localhost:1337";
-let socket;
-let editor;
+import Editor from './components/Editor';
 
 function Doc() {
     const { id } = useParams();
@@ -25,43 +20,14 @@ function Doc() {
 
         loadData();
     }, [id]);
-    
-    const [editorContent, setEditorContent] = useState(docu["content"]);
-    useEffect(() => {
-        editor = document.getElementById("texteditor");
-        editor.innerText = editorContent;
-    }, [editorContent])
 
-
-    useEffect(() => {
-        socket = io(SERVER_URL);
-
-        socket.emit("create", id);
-        socket.on("content", (data) => {
-            setEditorContent(data.content, false);
-        })
-        editor = document.getElementById("texteditor");
-
-        editor.addEventListener("keyup", function(event) {
-            socket.emit("content", {
-                _id: id,
-                content: event.target.value
-            });
-        })
-        return () => {
-            socket.disconnect();
-        }
-    }, [id]);
-
-
-    
     return (
         <>
         <Header />
         <main className="main">
             <h2>Dokument</h2>
             {load}
-            <Form doc={docu} />
+            <Editor doc={docu} />
         </main>
         <Footer />
         </>
---
+++ src/views/components/Editor.jsx
@@ -0,0 +1,120 @@
+import { useState, useEffect } from "react";
+import documents from "../models/docs";
+import { useNavigate } from "react-router-dom";
+import { io } from "socket.io-client";
+
+const SERVER_URL = "http://localhost:1337";
+let socket;
+
+function Editor(props) {
+    const navigate = useNavigate();
+    const [title, setNewTitle] = useState("");
+    const [content, setNewContent] = useState("");
+    const [editorContent, setEditorContent] = useState(props.doc["content"]);
+
+    function handleChange(e) {
+        e.preventDefault();
+        e.target.name == "title" ? setNewTitle(e.target.value) : setNewContent(e.target.value);
+    }
+
+    async function createDoc() {
+        let newDoc = {
+            "title": title === "" ? props.doc.title : title,
+            "content": content === "" ? props.doc.content : content
+        };
+        const result = await documents.addOneDoc(newDoc);
+
+        navigate(`/lucid-frontend/${result.insertedId}`); // Redirect to new id
+    }
+
+    async function updateDoc() {
+        let updatedDoc = {
+            "id": props.doc._id,
+            "title": title === "" ? props.doc.title : title,
+            "content": content === "" ? props.doc.content : content
+        };
+
+        await documents.updateOneDoc(updatedDoc);
+        
+        // Show success
+        const updateBtn = document.getElementById("update");
+        updateBtn.classList.add("success-animation");
+        setTimeout(() => updateBtn.classList.remove("success-animation"), 1000);
+    }
+
+    async function deleteDoc() {
+        if (props.doc._id) {
+            await documents.deleteOneDoc(props.doc._id);
+        }
+        
+        navigate("/lucid-frontend/");  // Redirect to home
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        switch (e.nativeEvent.submitter.value) {
+            case "Skapa":
+                await createDoc();
+                break;
+            case "Uppdatera":
+                await updateDoc();
+                break;
+            case "Radera":
+                await deleteDoc();
+                break;
+            default:
+        }
+    }
+
+    
+    useEffect(() => {
+        document.getElementById("texteditor").innerText = editorContent;
+    }, [editorContent])
+
+
+    useEffect(() => {
+        socket = io(SERVER_URL);
+
+        socket.emit("create", props.doc._id);
+
+        socket.on("content", (data) => {
+            setEditorContent(data.content, false);
+        });
+
+        document.getElementById("texteditor").addEventListener("keyup", function(event) {
+            socket.emit("content", {
+                id: props.doc._id,
+                title: props.doc.title,
+                content: event.target.value
+            });
+        });
+
+        return () => {
+            socket.disconnect();
+        }
+    }, [props.doc]);
+
+    return (
+        <>
+
+        <form onSubmit={handleSubmit} className="new-doc">
+            <label htmlFor="id">Id</label>
+            <input type="text" name="id" defaultValue={props.doc._id} readOnly/>
+
+            <label htmlFor="title">Titel</label>
+            <input type="text" name="title" defaultValue={props.doc.title} onChange={handleChange} />
+
+            <label htmlFor="content">Innehåll</label>
+            <textarea id="texteditor" name="content" defaultValue={props.doc.content} onChange={handleChange}></textarea>
+
+            <input type="submit" id="create" value="Skapa" />
+            <input type="submit" id="update" value="Uppdatera" />
+            <input type="submit" id="delete" value="Radera" />
+        </form>
+        
+        </>
+    );
+};
+
+export default Editor;
