584d1076513e38c1bffed90d17ee666114de7098
Ida Alin <dev.7rip7ych@gmail.com>
2025-10-02 14:16:19 +0200

    Implement socket

--- src/views/Doc.jsx
+++ src/views/Doc.jsx
@@ -1,10 +1,15 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect} from "react";
+import { io } from 'socket.io-client';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import imgUrl from '../assets/skateboard.gif';
 import documents from './models/docs';
 import Form from './components/Form';
+
+const SERVER_URL = "http://localhost:1337";
+let socket;
+let editor;
 
 function Doc() {
     const { id } = useParams();
@@ -20,6 +25,35 @@ function Doc() {
 
         loadData();
     }, [id]);
+    
+    const [editorContent, setEditorContent] = useState(docu["content"]);
+    useEffect(() => {
+        editor = document.getElementById("texteditor");
+        editor.innerText = editorContent;
+    }, [editorContent])
+
+
+    useEffect(() => {
+        socket = io(SERVER_URL);
+
+        socket.emit("create", id);
+        socket.on("content", (data) => {
+            setEditorContent(data.content, false);
+        })
+        editor = document.getElementById("texteditor");
+
+        editor.addEventListener("keyup", function(event) {
+            socket.emit("content", {
+                _id: id,
+                content: event.target.value
+            });
+        })
+        return () => {
+            socket.disconnect();
+        }
+    }, [id]);
+
+
     
     return (
         <>
