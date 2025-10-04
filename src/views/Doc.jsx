ce042511d3495d1ff7cc860008237e03af710145
Ida Alin <dev.7rip7ych@gmail.com>
2025-10-04 14:20:44 +0200

    Add button to switch between editors

--- src/views/Doc.jsx
+++ src/views/Doc.jsx
@@ -5,21 +5,34 @@ import Footer from './components/Footer';
 import imgUrl from '../assets/skateboard.gif';
 import documents from './models/docs';
 import Editor from './components/Editor';
+import CodeEditor from './components/CodeEditor';
 
 function Doc() {
     const { id } = useParams();
     const [docu, setDoc] = useState([]);
     const [load, setLoading] = useState(<img src={imgUrl} alt="loading" className="loading-gif" />);
+    const [editor, setEditor] = useState([]);
 
     useEffect(() => {
         const loadData = async () => {
             const DocData = await documents.getOneDoc(id);
             setDoc(DocData);
+            setEditor(<Editor doc={DocData} />);
             setLoading();
         };
 
         loadData();
     }, [id]);
+
+    function changeEditor(e) {
+        if (e.target.checked) {
+            console.log("code");
+            setEditor(<CodeEditor />);
+        } else {
+            console.log("text");
+            setEditor(<Editor doc={docu} />);
+        }
+    }
 
     return (
         <>
@@ -27,7 +40,10 @@ function Doc() {
         <main className="main">
             <h2>Dokument</h2>
             {load}
-            <Editor doc={docu} />
+            {editor}
+            <div className="slide-container">
+                <input type="checkbox" id="changeEditor" className="slide-checkbox change-editor" onClick={changeEditor} />
+            </div>
         </main>
         <Footer />
         </>
