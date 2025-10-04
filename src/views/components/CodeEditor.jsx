d0777430ebd226eaec70cfce58bdd9a5da968167
Ida Alin <dev.7rip7ych@gmail.com>
2025-10-04 15:12:51 +0200

    Add monaco editor to component

--- src/views/components/CodeEditor.jsx
+++ src/views/components/CodeEditor.jsx
@@ -1,9 +1,12 @@
+import Editor from "@monaco-editor/react";
+
 function CodeEditor() {
 
     return (
         <>
         <div className="code-editor">
             <h2>Code Editor</h2>
+            <Editor height="80vh" defaultLanguage="javascript" defaultValue="// write code here" />
         </div>
         </>
     );
