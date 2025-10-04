f61e984100124e5d072ad81bcc7b7ece8c3f56c4
Ida Alin <dev.7rip7ych@gmail.com>
2025-10-04 13:55:57 +0200

    Create base structure for CodeEditor

---
+++ src/views/components/CodeEditor.jsx
@@ -0,0 +1,12 @@
+function CodeEditor() {
+
+    return (
+        <>
+        <div className="code-editor">
+            <h2>Code Editor</h2>
+        </div>
+        </>
+    );
+};
+
+export default CodeEditor;
