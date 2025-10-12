a765753ec7b1980495d10cf8e6348f98381fa7bd
Ida Alin <dev.7rip7ych@gmail.com>
2025-10-12 15:26:54 +0200

    Divide Doc testing into different parts

--- src/tests/Doc.test.jsx
+++ src/tests/Doc.test.jsx
@@ -5,49 +5,57 @@ import { MemoryRouter } from "react-router-dom";
 
 
 describe('Doc', () => {
-    test('Testing if renders heading', () => {
-        //Need to render component with som type of router.
-        render(
-            <MemoryRouter>
-                <Doc />
-            </MemoryRouter>
-        );
+    describe('DOM', () => {
+        test('Testing if renders heading', () => {
+            //Need to render component with som type of router.
+            render(
+                <MemoryRouter>
+                    <Doc />
+                </MemoryRouter>
+            );
+            
+            const h2Element = screen.getByRole('heading', { level: 2 });
+            
+            expect(h2Element).toBeInTheDocument();
+            expect(h2Element).toHaveTextContent('Dokument');
+        });
+
+        test('Testing if component renders right labels', () => {
+            //Need to render component with som type of router.
+            render(
+                <MemoryRouter>
+                    <Doc />
+                </MemoryRouter>
+            );
+            
+            const label1Element = screen.getByText('Id');
+            const label2Element = screen.getByText('Titel');
+            const label3Element = screen.getByText('Innehåll');
+
+            expect(label1Element).toBeInTheDocument();
+            expect(label2Element).toBeInTheDocument();
+            expect(label3Element).toBeInTheDocument();
+        });
+
+        test('Testing if form have all the buttons', () => {
+            render(
+                <MemoryRouter>
+                    <Doc />
+                </MemoryRouter>
+            );
         
-        const h2Element = screen.getByRole('heading', { level: 2 });
-        
-        expect(h2Element).toBeInTheDocument();
-        expect(h2Element).toHaveTextContent('Dokument');
+            const buttons = screen.getAllByRole('button');
+            expect(buttons).toHaveLength(3);
+
+            expect(screen.getByRole('button', { name: 'Skapa' })).toBeInTheDocument();
+            expect(screen.getByRole('button', { name: 'Uppdatera' })).toBeInTheDocument();
+            expect(screen.getByRole('button', { name: 'Radera' })).toBeInTheDocument();
+        });
     });
 
-    test('Testing if component renders right labels', () => {
-        //Need to render component with som type of router.
-        render(
-            <MemoryRouter>
-                <Doc />
-            </MemoryRouter>
-        );
-        
-        const label1Element = screen.getByText('Id');
-        const label2Element = screen.getByText('Titel');
-        const label3Element = screen.getByText('Innehåll');
+    describe('Sockets', () => {});
 
-        expect(label1Element).toBeInTheDocument();
-        expect(label2Element).toBeInTheDocument();
-        expect(label3Element).toBeInTheDocument();
-    });
+    describe('Data', () => {});
 
-    test('Testing if form have all the buttons', () => {
-        render(
-            <MemoryRouter>
-                <Doc />
-            </MemoryRouter>
-        );
-    
-    const buttons = screen.getAllByRole('button');
-    expect(buttons).toHaveLength(3);
-
-    expect(screen.getByRole('button', { name: 'Skapa' })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: 'Uppdatera' })).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: 'Radera' })).toBeInTheDocument();
-    });
+    describe('Editor', () => {});
 });
