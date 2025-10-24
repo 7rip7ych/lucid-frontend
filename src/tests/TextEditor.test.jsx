3b8ea96160dd00e89110d89f1b961e9ecaf78ea4
Ida Alin <dev.7rip7ych@gmail.com>
2025-10-24 14:14:49 +0200

    Add tests for comment highlights

--- src/tests/TextEditor.test.jsx
+++ src/tests/TextEditor.test.jsx
@@ -2,36 +2,74 @@ import TextEditor from '../views/components/TextEditor';
 import { render, screen } from '@testing-library/react';
 import { describe, test, expect } from 'vitest';
 import { MemoryRouter } from "react-router-dom";
+import { beforeEach } from 'vitest';
+import { act } from '@testing-library/react';
 
 const testDoc = {
     id: 101,
     type: "text",
     title: "test",
-    content: "hello world",
-    owners: [{ email: "banan" }]
+    content: `line 1: hello world\nline 2: this is a\nline 3: multiline string`,
+    owners: [{ email: "banan@gmail.com" }]
 };
 
+const testComments = [
+    {
+        _id: 111,
+        selection: 1,
+        owner: { email: "test@gmail.com" },
+        content: "a test comment"
+    },
+    {
+        _id: 222,
+        selection: 2,
+        owner: { email: "test@gmail.com" },
+        content: "some other text"
+    },
+    {
+        _id: 333,
+        selection: 3,
+        owner: { email: "test@gmail.com" },
+        content: "miscellaneous content"
+    }
+];
+
 describe('Text Editor', () => {
-    test('Test if component renders DOM', () => {
-        render(
-            <MemoryRouter>
-                <TextEditor data={ testDoc } />
-            </MemoryRouter>
-        );
+    beforeEach(async() => {
+        await act( async() => {
+            render(
+                <MemoryRouter>
+                    <TextEditor data={ testDoc } comments={ testComments } />
+                </MemoryRouter>
+            );
+        });
         
+    });
+    
+    test('Test if component renders DOM', () => {
         expect(screen.getAllByRole('textbox')).toHaveLength(3);
         expect(screen.getAllByRole('button', { type: "submit" })).toHaveLength(3);
     });
 
     test('Test if render correct data', () => {
-        render(
-            <MemoryRouter>
-                <TextEditor data={ testDoc } />
-            </MemoryRouter>
-        );
-        
         expect(screen.getByDisplayValue(testDoc.id)).toBeInTheDocument();
         expect(screen.getByDisplayValue(testDoc.title)).toBeInTheDocument();
-        expect(screen.getAllByText(testDoc.content)).toHaveLength(2);
+        expect(screen.getAllByText(/^line 1.*multiline string/)).toHaveLength(1);
     });
+
+    
+    test('Testing if comments render', () => {
+        const highlight1 = screen.getByText('line 1: hello world');
+        const highlight2 = screen.getByText('line 2: this is a');
+        const highlight3 = screen.getByText('line 3: multiline string');
+
+        expect(highlight1).toBeInTheDocument();
+        expect(highlight2).toBeInTheDocument();
+        expect(highlight3).toBeInTheDocument();
+
+        expect(highlight1.getAttribute("data-id")).toBe(testComments[0]._id.toString());
+        expect(highlight2.getAttribute("data-id")).toBe(testComments[1]._id.toString());
+        expect(highlight3.getAttribute("data-id")).toBe(testComments[2]._id.toString());
+    });
+    
 });
