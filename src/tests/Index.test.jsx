1d36ae287e371a131696299ed8125a3045bd8eef
Ida Alin <dev.7rip7ych@gmail.com>
2025-10-24 14:31:03 +0200

    Add test for documents shown in index

--- src/tests/Index.test.jsx
+++ src/tests/Index.test.jsx
@@ -1,8 +1,24 @@
 import Index from '../views/Index';
-import { render, screen } from '@testing-library/react';
-import { describe, test, expect } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import { vi, describe, test, expect } from 'vitest';
 import { MemoryRouter } from 'react-router-dom';
 
+const testDocs = [
+    {
+        _id: 101,
+        type: "text",
+        title: "test title",
+        content: "hello world",
+        owners: [{ email: "banan@gmail.com" }]
+    },
+    {
+        _id: 10101,
+        type: "text",
+        title: "another title",
+        content: "hello world",
+        owners: [{ email: "banan@gmail.com" }]
+    }
+]
 describe('Index', () => {
     test('renders Index', () => {
         render(
@@ -15,5 +31,31 @@ describe('Index', () => {
         
         expect(h2Element).toBeInTheDocument();
         expect(h2Element).toHaveTextContent('dokument');
+    });
+
+    test('renders documents', async () => {
+        
+        vi.mock(import('../views/models/docs.jsx'), async() => {
+            return {
+                default: {
+                    allDocuments() {
+                        return testDocs;
+                    }
+                },
+                variable: 'mock',
+            }
+        });
+    
+        render(
+            <MemoryRouter>
+                <Index />
+            </MemoryRouter>
+        );
+        waitFor(() => {
+            expect(screen.getByText(testDocs[0].title)).toBeInTheDocument();
+            expect(screen.getByText(testDocs[1].title)).toBeInTheDocument();
+        });
+        
+        vi.resetAllMocks();
     });
 });
