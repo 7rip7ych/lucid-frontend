1c27844d16925cf671280628323ced278496b557
lucation <lucasdanielssonvalladares@gmail.com>
2025-10-24 23:35:08 -0500

    New component that shows info of one document, the option to navigate to edit the document and also sharing it with others

---
+++ src/views/components/DocInfo.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import shareDoc from '../models/shareDoc.jsx';
+import { Link } from 'react-router-dom';
+
+function DocInfo ({ docId, username, docTitle, docContent }) {
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [showForm, setShowForm] = useState(false);
+
+    async function handleMail(event) {
+        event.preventDefault();
+
+        const message = {
+            to: `${email}`,
+            // from: 'lucasdanielssonvalladares@gmail.com',
+            subject: `${username} delar ett dokument med dig`,
+            text: `Hej! ${username} vill dela ett dokument med dig. Dokument id: ${docId}. Om du inte redan är registrerad så kan du registrera dig på: https://7rip7ych.github.io/lucid-frontend/register`,
+            html: `<h1>Hej!</h1>
+            <p>${username} vill dela ett dokument med dig.</p>
+            <p>Dokument id: ${docId}</p>
+            <p>Om du inte redan är registrerad så kan du registrera dig här <a href="https://7rip7ych.github.io/lucid-frontend/register">här</a></p>
+            `
+        };
+
+        const result = await shareDoc.sendMail(message);
+
+        if(result.error) {
+            setMessage(<p style={{ color: 'red' }}>Error</p>);
+        }
+
+        alert(`Mailet skickades till ${email}`);
+
+        setShowForm(false);
+    }
+
+    function shareForm() {
+        return (
+            <>
+            <form onSubmit={handleMail} className='share-form'>
+
+            <p><label>Dela dokument med: </label></p>
+            <p>
+            <input className='share-text'
+                    type="email"
+                    value={email}
+                    placeholder='Email'
+                    onChange={(e) => setEmail(e.target.value)}
+            />
+            </p>
+            <input className="document-button" type="submit" value="Skicka"/>
+            <button className="document-button" onClick={() => setShowForm(false)}>
+                Stäng
+            </button>
+
+            </form>
+            {message}
+            </>
+        )
+    }
+
+    return (
+        <>
+        { !showForm ? (
+            <div key={docId} className="document">
+
+            <h3 key={docId}>{docTitle}</h3>
+
+            <div className="document-content">
+                <p>{docContent.slice(0, 40)} ...</p>
+            </div>
+
+            <Link to={docId} className="document-button">Skriv</Link>
+            <button className="document-button" onClick={() => setShowForm(true)}>
+                Dela
+            </button>
+
+            </div>
+            ) : (
+                shareForm()
+        )}
+        </>
+    )
+}
+
+export default DocInfo;
