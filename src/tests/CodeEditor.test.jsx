484f062d0fdde0bebe7a44fd903266d56dd2a9c6
Ida Alin <dev.7rip7ych@gmail.com>
2025-10-25 17:04:46 +0200

    Minor adjustments

--- src/tests/CodeEditor.test.jsx
+++ src/tests/CodeEditor.test.jsx
@@ -5,9 +5,9 @@ import { MemoryRouter } from "react-router-dom";
 
 const testDoc = {
     id: 101,
-    type: "text",
+    type: "code",
     title: "test",
-    content: `line 1: hello world\nline 2: this is a\nline 3: multiline string`,
+    content: "console.log('hello world');",
     owners: [{ email: "banan@gmail.com" }]
 };
 
@@ -17,18 +17,6 @@ const testComments = [
         selection: 1,
         owner: { email: "test@gmail.com" },
         content: "a test comment"
-    },
-    {
-        _id: 222,
-        selection: 2,
-        owner: { email: "test@gmail.com" },
-        content: "some other text"
-    },
-    {
-        _id: 333,
-        selection: 3,
-        owner: { email: "test@gmail.com" },
-        content: "miscellaneous content"
     }
 ];
 
@@ -43,14 +31,13 @@ describe('Code Editor', () => {
         });
     });
 
-
     test('Test if component renders DOM', () => {
         expect(screen.getAllByRole('textbox')).toHaveLength(1);
         expect(screen.getByText('Save')).toBeInTheDocument();
         expect(screen.getByText('Execute')).toBeInTheDocument();
     });
 
-    test('Test if render correct data', () => {
+    test('Test if render correct data', async() => {
         expect(screen.getByDisplayValue(testDoc.title)).toBeInTheDocument();
     });
 });
